06088c4bf69b8cbc81f58d813d8d19df5858e8dc
jackliu <liu20011122@126.com>
2013-08-13 22:03:39 +0800

    第七天 javaScript 脚本

---
+++ Day8-JQuery框架/动态插件/multipleJQueryRoundaboutShapes/fredhq-roundabout-shapes-v2.0-0-g1a17f0d/fredhq-roundabout-shapes-1a17f0d/js/projects/v2.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {	
+	$('ul#example-lazySusan').roundabout();
+	$('ul#example-waterWheel').roundabout({shape: 'waterWheel'});
+	$('ul#example-figure8').roundabout({shape: 'figure8'});
+	$('ul#example-square').roundabout({shape: 'square'});
+	$('ul#example-conveyorBeltLeft').roundabout({shape: 'conveyorBeltLeft'});
+	$('ul#example-conveyorBeltRight').roundabout({shape: 'conveyorBeltRight'});
+	$('ul#example-diagonalRingLeft').roundabout({shape: 'diagonalRingLeft'});
+	$('ul#example-diagonalRingRight').roundabout({shape: 'diagonalRingRight'});
+	$('ul#example-theJuggler').roundabout({shape: 'theJuggler'});
+	$('ul#example-goodbyeCruelWorld').roundabout({shape: 'goodbyeCruelWorld'});
+	$('ul#example-rollerCoaster').roundabout({shape: 'rollerCoaster'});
+	$('ul#example-tearDrop').roundabout({shape: 'tearDrop'});
+});
