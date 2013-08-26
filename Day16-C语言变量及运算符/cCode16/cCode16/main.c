462e630b0ca43f93caca3fbf94806f535b22680a
jackliu <liu20011122@126.com>
2013-08-26 16:55:40 +0800

    C运算符

--- Day16-C语言变量及运算符/cCode16/cCode16/main.c
+++ Day16-C语言变量及运算符/cCode16/cCode16/main.c
@@ -40,15 +40,94 @@ int main(int argc, const char * argv[])
     putchar(ch);
     */
     
+    /*
+    printf("练习三：运算符  \n");
+    int nNum1 = 10;
+    int nNum2 = 3;
+    
+    int nSum1  = nNum1 + nNum2; //13
+    int nSum2 = nNum1 + 20;
+    //使用上面定义好的类型
+    nSum1  = nSum1 - 5;  //13 - 5 = 8
+    //nSum1 -= 5;
+    
+    
+    int nA = nNum1 * nNum2;   //30
+    int nB = nNum1 / nNum2;   //10 / 3 = ?
+    printf("除法运算结果  %d \n",nB);
+    int nC = nNum1 % nNum2;   // 10 % 3 = ?
+    printf(" %d \n",nC);
+    
+    double dNum = nNum1 / nNum2;
+    printf("double 商: %f",dNum);
+     
+     
+    int a = 1;
+    a++;   //运行完毕，进行自增
+    ++a;   //运行前，进行自增
+    printf("a = %d",a);
+     */
+    
+    /*
+    if ( 4 > 2  &&  10 >9) {
+        printf("与 整体是真的 \n");
+    }else
+    {
+        printf("与 整体是假的 \n");
+    }
+    
+    if ( 1 > 2  ||  10 >9) {
+        printf("或 整体是真的 \n");
+    }else
+    {
+        printf("或 整体是假的 \n ");
+    }
+    */
+    
+    printf(" 交换两个变量的值 \n");
+    printf("方法一，借用第三变量 \n");
+    int a = 5; //0101
+    int b = 9; //1001
+    
+    /*
+    int temp = 0;  //第三个变量
+    temp = a;
+    a = b ;
+    b =temp;
+    printf("a = %d  , b = %d \n",a,b);
+    
+    printf("方法二，用异或运算符，两个变量交换值 \n");
+    a = a^b;  //a = 0101 ^ 1001 = 1100 =12
+    b = a^b;  //b = 1100 ^ 1001 = 0101 =5
+    a = a^b;  //a = 1100 ^ 0101 = 1001 =9
+    printf("a = %d  , b = %d \n",a,b);
+    
+    printf("方法二，用+ -运算符，两个变量交换值 \n");
+    a = a+b;  //a = 5 + 9 = 14
+    b = a-b;  //b = 14 -9 = 5
+    a = a-b;  //a = 14 -5 =9
+    printf("a = %d  , b = %d \n",a,b);
+     
+    
+    //适合用于只有两种结果的情况下
+    int result = (12>3)?30:(100-28);
+    printf("%d \n",result);
+    */
+    
+    
+    
+    
     
     //输入一个年份，判断是否是闰年
     printf("请输入一个年份 \n");
     int year;
     scanf("%d",&year);
-    
-    
-    
-    
+    //判断这个数据是否满足闰年的公式
+    if ((year %4 == 0 && year%100 !=0) || year %400 == 0) {
+        printf(" %d 是闰年 .",year);
+    }else{
+        printf(" %d 不是闰年 .",year);
+    }
     
     return 0;
 }
