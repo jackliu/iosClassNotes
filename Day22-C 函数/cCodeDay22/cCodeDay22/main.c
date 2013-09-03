db84896cbf89f718e352461b0ece225d26c62106
jackliu <liu20011122@126.com>
2013-09-03 16:16:13 +0800

    局部全局变量_形参实参

--- Day22-C 函数/cCodeDay22/cCodeDay22/main.c
+++ Day22-C 函数/cCodeDay22/cCodeDay22/main.c
@@ -7,12 +7,22 @@
 //
 
 #include <stdio.h>
+#include "code.h"
+
+//在{} 作用域外部定义的变量，作用域是  ?
+int num=20;
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    //show();
+    
+    int num = 10;
+    
+    print(num);
+    
+    printf("num = %d \n",num);
+    
     return 0;
 }
 
